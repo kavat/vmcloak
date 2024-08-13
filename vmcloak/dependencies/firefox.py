a46fdb020a95e9b556086d042bc243b537bfc024
Andrea Cavallini <andrycavallini87@gmail.com>
2024-08-13 10:37:05 +0200

    added new firefox

--- vmcloak/dependencies/firefox.py
+++ vmcloak/dependencies/firefox.py
@@ -7,7 +7,7 @@ from vmcloak.abstract import Dependency
 
 class Firefox(Dependency):
     name = "firefox"
-    default = "41.0.2"
+    default = "129.0"
     tags = ["browser_firefox"]
     exes = [{
         "version": "41.0.2",
@@ -21,12 +21,16 @@ class Firefox(Dependency):
         "version": "63.0.3",
         "url": "https://cuckoo.sh/vmcloak/firefox_63_0_3.exe",
         "sha1": "c5f03fc93aebd2db9da14ba6eb1f01e98e18d95b",
+    }, {
+        "versione": "129.0",
+        "url": "https://download-installer.cdn.mozilla.net/pub/firefox/releases/129.0/win64/en-US/Firefox%20Setup%20129.0.exe",
+        "sha1": "3220951095d67703c14543ee4abfe55daf05165a"
     }]
 
     def run(self):
-        self.upload_dependency("C:\\Firefox_Setup_41.0.2.exe")
-        self.a.execute("C:\\Firefox_Setup_41.0.2.exe -ms")
-        self.a.remove("C:\\Firefox_Setup_41.0.2.exe")
+        self.upload_dependency("C:\\%s" % self.filename)
+        self.a.execute("C:\\%s -ms" % self.filename
+        self.a.remove("C:\\%s" % self.filename)
 
 class Firefox41(Firefox, Dependency):
     """Backwards compatibility"""
